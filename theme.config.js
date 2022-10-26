0cf50dca421bf99b921fe989e41a3c0beb197ff1
Ivan Ljubicic <ivan@tvrtko.local>
2022-10-26 15:35:25 +0200

    popravljeno linkanje

--- theme.config.js
+++ theme.config.js
@@ -16,15 +16,11 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="description" content="Sve na jednom mjestu o biciklističkim popravcima" />
+      <meta name="og:description" content="Sve na jednom mjestu o biciklističkim popravcima" />
+      <meta name="og:title" content="Bike KSET wiki" />
+      <meta name="og:image" href="/bikelogo.png" />
+      <meta name="apple-mobile-web-app-title" content="Bike KSET wiki" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
